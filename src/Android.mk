2c86e7c5d8d95240c77f4b497b12ca55ef0222cc
Martin Gerhardy <martin.gerhardy@gmail.com>
2013-09-27 16:52:56 +0200

    * use the original android.mk file from sdl2 with some minor modifications

--- src/Android.mk
+++ src/Android.mk
@@ -2,31 +2,24 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE      := main
+LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
 SDL_IMAGE_PATH := ../SDL_image
 SDL_MIXER_PATH := ../SDL_mixer
 SDL_NET_PATH := ../SDL_net
 
-NAME := myawesomegame
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
+	$(LOCAL_PATH)/$(SDL_IMAGE_PATH)/include \
+	$(LOCAL_PATH)/$(SDL_MIXER_PATH)/include \
+	$(LOCAL_PATH)/$(SDL_NET_PATH)/include
 
-LOCAL_SRC_FILES := \
-	$(subst jni/src/,,$(wildcard jni/src/*.cpp)) \
-	\
-	$(SDL_PATH)/src/main/android/SDL_android_main.c
-
-LOCAL_CFLAGS :=
-
-LOCAL_C_INCLUDES  :=
-LOCAL_C_INCLUDES  += $(LOCAL_PATH)
-LOCAL_C_INCLUDES  += $(LOCAL_PATH)/libs
-LOCAL_C_INCLUDES  += $(LOCAL_PATH)/libs/SDL/include
-LOCAL_C_INCLUDES  += $(LOCAL_PATH)/libs/SDL_image
-LOCAL_C_INCLUDES  += $(LOCAL_PATH)/libs/SDL_mixer
-LOCAL_C_INCLUDES  += $(LOCAL_PATH)/libs/SDL_net
+# Add your application source files here...
+LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
+	$(subst jni/src/,,$(wildcard jni/src/*.cpp))
 
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_net
-LOCAL_LDLIBS           := -lGLESv2 -llog -lz -lm
+
+LOCAL_LDLIBS := -lGLESv1_CM -llog -lz -lm
 
 include $(BUILD_SHARED_LIBRARY)
